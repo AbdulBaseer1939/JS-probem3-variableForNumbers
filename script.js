1dc881896a8aab67155f34f0d6fcb6ba7136b6dc
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-28 21:59:12 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// problem#1: Declare a variable called age & assign to it your age. Show your age in an alert box.
+var age = 30;
+alert("age is: " + age);
+
+// problem#2:  Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+var visits = 0;
+visits++;
+alert("You have visited this site " + visits + " time.");
+document.body.innerHTML = `<p>you have visited this site ${visits} times.</p>`
